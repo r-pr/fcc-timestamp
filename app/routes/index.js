ee6cf534e6df9f8128843ea3852fe5024cea69ef
Ruslan Prokopenko <rus@lmg.kiev.ua>
2016-06-02 18:08:57 +0000

    removed unnecessary code from clementine.js boilerplate

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,57 +1,27 @@
 'use strict';
-
-var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
+var marked = require('marked');
+var fs = require('fs');
 
 module.exports = function (app, passport) {
 
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
-	}
-
-	var clickHandler = new ClickHandler();
-
 	app.route('/')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/index.html');
-		});
-
-	app.route('/login')
 		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
+			var readme = fs.readFile(process.cwd() + '/README.md', 'utf-8', function(err, data){
+				if (!err) {
+					var parsed = marked(data);
+					res.send(parsed);
+				}
+				else {
+					console.log(process.cwd());
+					res.send('error');
+				}
+			})
 		});
 
-	app.route('/logout')
+	app.route('/:param')
 		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
+			res.send('invoking API for '+ req.params.param);
 		});
 
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
-
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
-
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
-
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
-
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
+	
 };
