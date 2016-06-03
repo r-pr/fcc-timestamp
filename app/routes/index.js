82d6bd71875f364aeff8d9759803fd712e878c7e
Ruslan Prokopenko <rus@lmg.kiev.ua>
2016-06-03 15:02:22 +0000

    finished routes. app is done

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,5 @@
 'use strict';
+//module for parsing markdown
 var marked = require('marked');
 var fs = require('fs');
 
@@ -12,7 +13,6 @@ module.exports = function (app, passport) {
 			
 			fs.readFile(process.cwd() + '/README.md', 'utf-8', function(err, readme){
 				if (!err) {
-					//parsing markdown.
 					var parsedReadme = marked(readme);
 					
 					fs.readFile(process.cwd() + '/views/layout.html', 'utf-8', function(err, layout){
@@ -20,19 +20,60 @@ module.exports = function (app, passport) {
 							var output = layout.replace(/<placeholder\/>/, parsedReadme);
 							res.send(output);
 						}	
+						else {
+							res.status(500).send('Error loading layout file.')
+						}
 					});
-				//	res.send(parsedReadme);
 				}
 				else {
-					console.log(process.cwd());
-					res.send('error');
+					res.status(500).send('Error loading readme file.');
 				}
 			})
 		});
 
-	app.route('/:param')
+	app.route('/:requestString')
 		.get(function (req, res) {
-			res.send('invoking API for '+ req.params.param);
+			
+			var requestString = req.params.requestString;
+			var result = {};
+			var monthNames = ["January", "February", "March", "April", "May", "June",
+				"July", "August", "September", "October", "November", "December"
+				];
+			
+			/* if timestamp is given */
+			if (/^-?\d+$/.test(requestString)) {
+				var date = new Date(parseInt(requestString)*1000);
+				var day = date.getDate();
+				var month = monthNames[date.getMonth()];
+				var year = date.getFullYear();
+				result.unix = parseInt(requestString);
+				result.natural = month + ' ' + day + ', ' + year;
+				res.json(result);
+			}
+			else {
+				var dateRegexp = /^([A-za-z]{3,})\s+(\d{1,2}),\s+(\d{4})\s*$/;
+				var match = requestString.match(dateRegexp);
+				
+				/* if natural language date is given and conforms to the rules */
+				if (match && match.length === 4 && //if we successfully get all 3 parameters and
+					monthNames.indexOf(match[1]) !== -1 && //if we have proper month name
+					+match[2] > 0 && //and if day of the month is within reasonable range
+					+match[2] <= 31
+					) {
+						var date = new Date(match[1]+' '+match[2]+', '+match[3]);
+						result.unix = date.getTime();
+						//I create date obj from given params to be able to convert February 31 to March 2
+						result.natural = monthNames[date.getMonth()]+' '+date.getDate()+', '+date.getFullYear();
+						res.json(result);
+					}
+				/* if request is wrong */
+				else {
+					result.unix = null;
+					result.natural = null;
+					res.json(result);
+				}
+			}
+			
 		});
 
 	
